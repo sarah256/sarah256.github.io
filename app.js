c1b50060e119be33f01eee60722593a270f624d3
sarah256 <srieger256@gmail.com>
2017-12-06 01:19:20 -0500

    added resume and links

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-  window.onload = function(){
-		typeWriter();
+ //  window.onload = function(){
+	// 	typeWriter();
 
-	}
+	// }
 
 var i = 0;
 var txt = '> Hello, world!'; /* The text */
 var speed = 200; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("helloworld").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("helloworld").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+// $('.one-time').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
