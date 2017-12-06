aec7acc6d7836294ae4b7687ca3304aab50645ed
sarah256 <srieger256@gmail.com>
2017-12-06 03:14:20 -0500

    added projects and portfolio

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
- //  window.onload = function(){
-	// 	typeWriter();
+//   window.onload = function(){
 
-	// }
+// 	// Get the modal
+// 	var modal = document.getElementById('myModal');
 
-var i = 0;
-var txt = '> Hello, world!'; /* The text */
-var speed = 200; /* The speed/duration of the effect in milliseconds */
+// 	// Get the image and insert it inside the modal - use its "alt" text as a caption
+// 	var img = document.getElementById('myImg');
+// 	var modalImg = document.getElementById("img01");
+// 	var captionText = document.getElementById("caption");
+// 	img.onclick = function(){
+// 	    modal.style.display = "block";
+// 	    modalImg.src = this.src;
+// 	    captionText.innerHTML = this.alt;
+// 	}
 
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("helloworld").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
+// 	// Get the <span> element that closes the modal
+// 	var span = document.getElementsByClassName("close")[0];
+
+// 	// When the user clicks on <span> (x), close the modal
+// 	span.onclick = function() { 
+// 	  modal.style.display = "none";
+// 	}
+
 // }
-
-// $('.one-time').slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 1,
-//   adaptiveHeight: true
-// });
